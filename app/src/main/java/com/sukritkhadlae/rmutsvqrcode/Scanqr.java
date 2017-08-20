4ab82f3f9cc1756a116d1dcf8ac7b179f0007f23
delta1994 <felta1994@gmail.com>
2017-08-20 15:33:32 +0700

    dsadasdas

---
+++ app/src/main/java/com/sukritkhadlae/rmutsvqrcode/Scanqr.java
@@ -0,0 +1,13 @@
+package com.sukritkhadlae.rmutsvqrcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scanqr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanqr);
+    }
+}
